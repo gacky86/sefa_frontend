54255f7e6659d67edb8bf76e9918d94defa45592
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2025-04-19 11:30:54 +0900

    feat: headerコンポーネントを作成 #41

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,14 +1,37 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import Logo from "assets/SEFA.svg?react";
+
+// Redux
+import { useSelector } from 'react-redux';
+import { RootState } from 'store/index';
+
 
 const Header = () => {
+  const { isSignedIn } = useSelector((state: RootState) => state.auth);
+
   return (
-    <div>
-      <Link to='/signin'>
-        ログイン
-      </Link>
-      <Link to='/singup'>
-        新規登録
-      </Link>
+    <div className="h-[50px] bg-white border-b-1 border-gray-300 flex justify-between">
+      <div>
+        <Link to="/">
+          <Logo className="w-[50px] h-[50px]"/>
+        </Link>
+      </div>
+      {}
+      {isSignedIn ? (
+        <div className="text-base mr-3 pt-3 text-auqa-blue">
+          <p>本日の学習: 104枚</p>
+        </div>
+      ) : (
+        <div className="text-base mr-3 pt-3">
+          <Link to='/signin' className="text-auqa-blue p-3">
+            ログイン
+          </Link>
+          <Link to='/singup' className="bg-auqa-blue text-white px-2 py-1 rounded-[5px] border-1 border-dark-navy-blue">
+            新規登録
+          </Link>
+        </div>
+      )}
+
     </div>
   )
 }
