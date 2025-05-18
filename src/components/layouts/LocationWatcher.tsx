89dc5ca6b67e1abb4d0af9ce44822a0632f685ba
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2025-05-18 17:32:59 +0900

    bug: LocationWatcherコンポーネントに'/fc-learning'から別ページに遷移した際の処理を追加 #131

--- src/components/layouts/LocationWatcher.tsx
+++ src/components/layouts/LocationWatcher.tsx
@@ -1,4 +1,5 @@
 import { closeModal } from "store/modalSlice";
+import { clearFCLearning } from "store/fcLearningSlice";
 import { useLocation } from 'react-router-dom'
 import { useEffect, useRef } from 'react';
 import { useDispatch } from "react-redux";
@@ -8,12 +9,22 @@ const LocationWatcher = () => {
   const prevPath = useRef(location.pathname);
   const dispatch = useDispatch();
 
-  // トップページから他のpathに遷移した際に、モーダル表示をリセットするためのuseEffect
   useEffect(() => {
-    if(prevPath.current === '/' && location.pathname !== '/') {
+    const currentPath = location.pathname;
+    const previousPath = prevPath.current;
+
+    // '/' から別ページに遷移したとき
+    if (previousPath === '/' && currentPath !== '/') {
       dispatch(closeModal());
     }
-    prevPath.current = location.pathname;
+
+    // '/fc-learning' から別ページに遷移したとき
+    if (previousPath === '/fc-learning' && currentPath !== '/fc-learning') {
+      dispatch(clearFCLearning());
+    }
+
+    // 最後に前のパスを更新
+    prevPath.current = currentPath;
   }, [location.pathname]);
 
   return null;
--- src/lib/api/bookmarkVideo.ts
+++ src/lib/api/bookmarkVideo.ts
@@ -1,43 +1,25 @@
 import { BookmarkVideo, BookmarkVideoParams } from "interfaces/index";
 import client from 'lib/api/client';
-import { getUserAuthHeader } from "lib/api/client";
-
 
 // 一覧
 export const getBookmarkVideosList = () => {
-  const authHeader = getUserAuthHeader();
-  return client.get<BookmarkVideo[]>('/bookmark_videos', {headers: authHeader});
+  return client.get<BookmarkVideo[]>('/bookmark_videos');
 };
-
 
 // 新規作成
 export const createBookmarkVideo = (params: BookmarkVideoParams) => {
-  const authHeader = getUserAuthHeader();
-  return client.post('/bookmark_videos', params, { headers: authHeader });
+  return client.post('/bookmark_videos', params);
 };
 
 
 // 削除
 export const deleteBookmarkVideo = (id: number) => {
-  const authHeader = getUserAuthHeader();
-  return client.delete(`/bookmark_videos/${id}`, {headers: authHeader});
+  return client.delete(`/bookmark_videos/${id}`);
 };
 
 // 指定したvideoIdを元に、currentUserが持つbookmarkVideoの中からvideoIdが一致するものがあるかどうかをチェックする
 // 一致するものがある場合は、ブックマーク済みとしてtrueを返す
 // それ以外の場合はfalseを返す
 export const checkBookmarkedVideo = (videoId: number) => {
-  const authHeader = getUserAuthHeader();
-  return client.get('/bookmark_videos/check_bookmarked_video', {headers: authHeader, params: { video_id: videoId }});
+  return client.get('/bookmark_videos/check_bookmarked_video', {params: { video_id: videoId }});
 }
-
-// 詳細
-// export const getBookmarkVideoDetail = (id: number) => {
-//   return client.get<BookmarkVideo>(`/bookmark_videos/${id}`);
-// };
-
-// 更新
-// export const updateBookmarkVideo = (id: number, params: BookmarkVideo) => {
-  //   const authHeader = getUserAuthHeader();
-  //   return client.patch(`/bookmark_videos/${id}`, params, { headers: authHeader });
-  // };
